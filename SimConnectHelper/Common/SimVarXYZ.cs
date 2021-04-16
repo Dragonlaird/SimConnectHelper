09f93b77f3d4d09c32f4986723adf28bfef14944
Jim Reeves <dragonlaird@msn.com>
2021-04-16 07:43:31 +0100

    Added 2 new strucutures for specific SimVar types. Modified Proj files to bundle SimConnect.dll's into a NUGET file

---
+++ SimConnectHelper/Common/SimVarXYZ.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimConnectHelper.Common
+{
+    public struct SimVarXYZ
+    {
+        public double x;
+        public double y;
+        public double z;
+    };
+}
